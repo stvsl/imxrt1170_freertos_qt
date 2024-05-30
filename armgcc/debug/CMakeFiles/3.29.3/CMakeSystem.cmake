7e166fa0fe5cba199b22a714ff36f4046970c6a9
stvsl <hongyu.zhang@dotrustech.com>
2024-05-30 10:27:18 +0800

    清理编译缓存

--- armgcc/debug/CMakeFiles/3.29.3/CMakeSystem.cmake
+++ armgcc/debug/CMakeFiles/3.29.3/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.8.9-zen1-1-zen")
+set(CMAKE_HOST_SYSTEM "Linux-6.9.2-zen1-1-zen")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.8.9-zen1-1-zen")
+set(CMAKE_HOST_SYSTEM_VERSION "6.9.2-zen1-1-zen")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/stvsl/Downloads/SDK_2_15_100_MIMXRT1170-EVK_ALL_TOOLCHAN/tools/cmake_toolchain_files/armgcc.cmake")
